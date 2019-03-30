25eced133c8aabcd477f3585b16fca414c5c5299
lilytran997 <lytran997@gmail.com>
2019-03-30 14:41:16 +0700

    first commit

---
+++ app/src/main/java/com/example/androidbeginer/ScollActivity.kt
@@ -0,0 +1,13 @@
+package com.example.androidbeginer
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ScollActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scoll)
+    }
+
+}
